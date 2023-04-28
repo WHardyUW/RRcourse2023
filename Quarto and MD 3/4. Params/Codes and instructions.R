b880ce937a545cc6acd2a729e7a4dd8cff2f8eb3
Nichakorn Pansailom <n.pansailom@student.uw.edu.pl>
2023-04-29 01:21:41 +0200

    Edit file: Codes and instructions.R by creating variable array of season 1-8 and data_name array and added for loop to automatise rendering quarto. but getting error message Error in knit_params_get(input_lines, params) :   render params not declared in YAML: printcode Calls: .main -> execute -> <Anonymous> -> knit_params_get Execution halted Error in : ! System command 'quarto.exe' failed

--- Quarto and MD 3/4. Params/Codes and instructions.R
+++ Quarto and MD 3/4. Params/Codes and instructions.R
@@ -1,23 +1,37 @@
 # Rendering
 library(quarto)
 
-quarto_render("QMD_class_3_4.qmd", execute_params = list(
-  year = 2017,
-  region = "Asia",
-  printcode = FALSE,
-  data = "file.csv"
-))
+# quarto_render("QMD_class_3_4.qmd", execute_params = list(
+#   year = 2017,
+#   region = "Asia",
+#   printcode = FALSE,
+#   data = "file.csv"
+# ))
+
+ss <- c(1:8)
+for (i in 1:8) {
+  data_array[i] <- paste0("../Data/season_", i, ".RData")
+}
+
+for (i in 1:8) {
+  quarto_render("Assignment.qmd", execute_params = list(
+   season = ss[i],
+   data = data_array[i],
+   printcode = FALSE
+   #data = "file.csv"
+), output_file = paste0("Report-GOT_season_", ss[i], ".html"))
+}
 
 # Name change
 
-reg <- "Asia"
-y <- 2049
+# reg <- "Asia"
+# y <- 2049
 
-quarto_render("QMD_class_3_4.qmd", execute_params = list(
-  year = y,
-  region = reg,
-  printcode = FALSE,
-  data = "file.csv"
-), output_file = paste0("Report-", reg, "-", y, ".html"))
+# quarto_render("QMD_class_3_4.qmd", execute_params = list(
+#   season = y,
+#   region = reg,
+#   printcode = FALSE,
+#   data = "file.csv"
+# ), output_file = paste0("Report-", reg, "-", y, ".html"))
 
 

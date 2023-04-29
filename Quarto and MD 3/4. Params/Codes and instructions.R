80696aefdb9548042bba2260b0bb6dff3ce963df
Nichakorn Pansailom <n.pansailom@student.uw.edu.pl>
2023-04-29 22:23:22 +0200

    final submission. Debugged the previous submission. editted the file path and successful generate the GOT report all 8 seasons to html.

--- Quarto and MD 3/4. Params/Codes and instructions.R
+++ Quarto and MD 3/4. Params/Codes and instructions.R
@@ -1,26 +1,24 @@
 # Rendering
 library(quarto)
+quarto_render("QMD_class_3_4.qmd", execute_params = list(
+  year = 2017,
+  region = "Asia",
+  printcode = FALSE,
+  data = "file.csv"
+))
 
-# quarto_render("QMD_class_3_4.qmd", execute_params = list(
-#   year = 2017,
-#   region = "Asia",
-#   printcode = FALSE,
-#   data = "file.csv"
-# ))
-
-ss <- c(1:8)
-for (i in 1:8) {
-  data_array[i] <- paste0("../Data/season_", i, ".RData")
-}
+hh <- c()
 
 for (i in 1:8) {
+  data_array <- paste0("D:/Quantitative/RR_lab/RR_lab4_1/Data/season_", i, ".RData")
   quarto_render("Assignment.qmd", execute_params = list(
-   season = ss[i],
-   data = data_array[i],
-   printcode = FALSE
-   #data = "file.csv"
-), output_file = paste0("Report-GOT_season_", ss[i], ".html"))
-}
+    season = i, data = data_array), output_file = paste0("Report-GOT_season_", i, ".html"))
+
+  }
+
+# for (i in 1:8) {
+# 
+# }
 
 # Name change
 

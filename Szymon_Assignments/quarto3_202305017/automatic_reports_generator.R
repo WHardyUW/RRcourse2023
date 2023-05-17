034263eaa31fbd3121bd0d4c806650e57dc5a97d
Szymon Socha <61253684+szymonsocha@users.noreply.github.com>
2023-05-17 11:14:16 +0200

    add Quarto3 2023-05-17 assignment

---
+++ Szymon_Assignments/quarto3_202305017/automatic_reports_generator.R
@@ -0,0 +1,16 @@
+library(quarto)
+
+seasons <- c(1:8)
+
+for (season in seasons) {
+  path <- paste0("../../Data/season_", season, ".csv")
+  
+  quarto_render("Assignment.qmd", execute_params = list(
+    season = season,
+    data = path
+  ), output_file = paste0("report-season_", season, ".html"))
+}
+
+
+
+

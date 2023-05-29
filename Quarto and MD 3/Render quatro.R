5a060ce40293834d329ecee5886c5d1a8248f9d3
sakibmasood <mmasood@unifonic.com>
2023-05-29 11:38:29 +0200

    adding Quarto assigmnet 3

---
+++ Quarto and MD 3/Render quatro.R
@@ -0,0 +1,14 @@
+library(quarto)
+
+
+
+setwd("C:\\Users\\HP\\Desktop\\Study\\semester_4\\ReproducibleResearch\\RRcourse2023\\Quarto and MD 3")
+
+for (i in 1:8){
+  quarto_render("Assignment.qmd", execute_params = list(
+    season = i), 
+    output_file = paste0("Report-",i,".html"))
+}
+
+
+

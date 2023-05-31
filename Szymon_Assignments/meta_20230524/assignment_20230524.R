f9249b7d545161a658d895fa8e10f141b26b03aa
SS <s.socha2@student.uw.edu.pl>
2023-05-31 10:44:31 +0200

    assignment meta_20230524 continued

--- Szymon_Assignments/meta_20230524/assignment_20230524.R
+++ Szymon_Assignments/meta_20230524/assignment_20230524.R
@@ -3,17 +3,44 @@ library(dplyr)
 
 data <- readxl::read_xlsx("../../13. Metaanalysis/data/metaanalysis_data.xlsx")
 
-m.raw <- metacont(n.e=N_boys,
+meta_boys <- metacont(n.e=N_boys,
                   mean.e=Mean_boys_play_male,
                   sd.e=SD_boys_play_male,
-                  n.c=N_girls,
-                  mean.c=Mean_girls_play_male,
-                  sd.c=SD_girls_play_male,
+                  n.c=N_boys,
+                  mean.c=Mean_boys_play_female,
+                  sd.c=SD_boys_play_female,
                   data=data,
                   studlab=paste(Study),
                   comb.fixed = TRUE,
                   comb.random = TRUE,
 )
-m.raw
 
-m.raw %>% forest(sortvar=TE)
+meta_girls <- metacont(n.e=N_girls,
+                      mean.e=Mean_girls_play_male,
+                      sd.e=SD_girls_play_male,
+                      n.c=N_girls,
+                      mean.c=Mean_girls_play_female,
+                      sd.c=SD_girls_play_female,
+                      data=data,
+                      studlab=paste(Study),
+                      comb.fixed = TRUE,
+                      comb.random = TRUE,
+)
+
+meta_boys %>% forest(sortvar=TE)
+
+meta_girls %>% forest(sortvar=TE)
+
+
+contour_levels <- c(0.90, 0.95, 0.99)
+contour_colors <- c("darkblue", "blue", "lightblue")
+
+meta_boys %>% funnel(contour = contour_levels, col.contour = contour_colors)
+legend("topright", c("p < 0.10", "p < 0.05", "p < 0.01"), bty = "n", fill = contour_colors)
+
+meta_girls %>% funnel(contour = contour_levels, col.contour = contour_colors)
+legend("topright", c("p < 0.10", "p < 0.05", "p < 0.01"), bty = "n", fill = contour_colors)
+
+
+meta_boys %>% metareg(~ `Parent present`)
+
